b683699aabc422ea4df81027145ae7df4f0e5612
Gustavo Fadel <gustavo.fadel@exactsales.com.br>
2025-05-11 20:49:44 -0300

    feat: add electron

---
+++ electron/main.ts
@@ -0,0 +1,30 @@
+import { app, BrowserWindow } from 'electron';
+import path from 'path';
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+    icon: path.join(__dirname, '/waves.ico'), 
+  });
+
+  if (!app.isPackaged) {
+    win.loadURL('http://localhost:5173');
+  } else {
+    win.loadFile(path.join(__dirname, '../renderer/index.html'));
+  }
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
